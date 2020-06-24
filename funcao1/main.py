7775a610ca34283df433ec8ac644a01a42c6750c
Vitor Vidal <vitorvidal.dev@gmail.com>
2020-06-24 19:39:07 -0300

    atualizacao completa do codigo

--- funcao1/main.py
+++ funcao1/main.py
@@ -1,72 +1,89 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib as plt
+import random
+import math
+from bitstring import BitArray
+
+# A classe GeneticAlgorithm é instânciada ao executar o arquivo.
+# população = X indivíduos randômicos, levando em conta os limites superior e inferior estabelecidos.
+# Conversão do fenótipo para genótipo. Separa-se a parte decimal da parte inteira de cada número e
+# aplica-se uma conversão inteiro->binário para cada indivíduo. As strings resultantes são concatenadas.
+# Conversão de genótipo para fenótipo ocorre de maneira análoga. A população anterior é sobrescrita
+
+class GeneticAlgorithm():
+
+    population = []
+    genotype = []
+    numeric_fitness = []
+
+    tuple_x_limits = (-20, 20)
 
 
-def CalculaFitness(x):
-    y = np.sin(x) + np.cos(x * np.sqrt(3))
-    return round(y, 4)
+    def __init__(self):
+        print("Class instance created")
 
 
-def mutate(parents, funcao_fitness):
-    n = int(len(parents))
-    scores = funcao_fitness(parents)
-    scores = scores[scores > -2]
-    parents = np.array(parents)[scores > -2]
+    def generate_population(self, population_size):
+        ''' Generates a random list of floating point numbers, given the range and the list size.'''
 
-    children = np.random.choice(parents, size=n)
-    return children.tolist()  # retorna o eixo x após a mutação
+        lower_limit, upper_limit = self.tuple_x_limits
 
-
-def GA(parents, funcao_fitness, numero_geracoes=1000):
-    histograma = []
-    # geracao zero
-    melhor_pai, melhor_fitness = GetFittestParent(
-        parents, funcao_fitness)  # extrai o individuo mais apto. (parents = posições aleatórias no eixo x, fitness = função da iteração)
-
-    # plota o grafico da função
-    x = np.linspace(start=-20, stop=20, num=1000)
-    plt.plot(x, funcao_fitness(x))
-    plt.scatter(parents, funcao_fitness(parents), marker='o')
-
-    # próximas gerações
-    for i in range(1, numero_geracoes):
-        parents = mutate(parents, funcao_fitness=funcao_fitness)
-
-        pai_atual, fitness_atual = GetFittestParent(
-            parents, funcao_fitness)
-
-        # atualiza os melhores valores de fitness
-        if fitness_atual < melhor_fitness:
-            melhor_fitness = fitness_atual
-            melhor_pai = pai_atual
-
-        pai_atual, fitness_atual = GetFittestParent(
-            parents, funcao_fitness)
-        # salva a menor coordenada
-        histograma.append((i, np.min(funcao_fitness(parents))))
-
-    plt.scatter(parents, funcao_fitness(parents))
-    plt.scatter(melhor_pai, funcao_fitness(
-        melhor_pai), marker='.', c='b', s=200)
-    plt.show()
-
-    print('generation {}| best fitness {}| melhor_pai {}'.format(
-        i, melhor_fitness, melhor_pai))
-
-    return melhor_pai, melhor_fitness, histograma
+        for i in range(population_size):
+            individual = random.uniform(lower_limit, upper_limit).__round__(4)
+            self.population.append(individual)
 
 
-def GetFittestParent(parents, fitness):
-    _fitness = fitness(parents)
-    # lista de coordenas (x, y) da função
-    PFitness = list(zip(parents, _fitness))
-    # ordena a lista de coordenadas em ordem crescente de acordo com a variável do eixo x
-    PFitness.sort(key=lambda x: x[1])
-    melhor_pai, melhor_fitness = PFitness[0]
-    return round(melhor_pai, 4), round(melhor_fitness, 4)
+    def fenotype_to_genotype(self):
+        '''Converts the population values to a binary representation'''
+        
+        for individual in self.population:
+            integer, dec = str(individual).split('.')
+            self.genotype.append(str(np.binary_repr(int(integer), width=6) + np.binary_repr(int(dec), width=14)))
 
 
-fitness = np.vectorize(CalculaFitness)
-tamanho_populacao = np.random.uniform(low=-20, high=20, size=5)
-pai_, fitness_, histograma_ = GA(tamanho_populacao, fitness)
-print('melhor pai {}, melhor fitness {}'.format(pai_, fitness_))
+    def genotype_to_fenotype(self):
+        '''Convets the binary string back to a floating point number. Overrides the previous population value'''
+
+        new_fenotype = []
+
+        for individual in self.genotype:
+            integer_section = individual[:6]
+            dec_section = individual[6:]
+
+            integer_str = str(int(BitArray(bin=integer_section).int))
+            dec_str = str(int(dec_section, 2))
+
+            concat = integer_str + '.' + dec_str
+            new_fenotype.append(float(concat))
+
+        self.population = new_fenotype
+
+
+    def fitness_function(self):
+        ''' Creates a list from the fitness value for each individual '''
+        self.numeric_fitness = [round(math.sin(i) + math.cos(i * math.sqrt(3)), 4) for i in self.population]
+
+
+    def crossover(self):
+        ''' TODO: Implement '''
+
+    def mutation(self):
+        ''' TODO: Implement '''
+
+    def roulette_selection(self):
+        ''' TODO: Implement '''
+        
+
+
+    def print_table(self):
+        for i in range(len(self.population)):
+            print('individual: {} , genotype: {} , fitness: {}\n'.format(self.population[i], self.genotype[i], self.numeric_fitness[i]))
+
+if __name__ == '__main__':
+    ga = GeneticAlgorithm()
+    ga.generate_population(100)
+    ga.fenotype_to_genotype()
+    ga.genotype_to_fenotype()
+    ga.fitness_function()
+    ga.roulette_selection()
+    ga.print_table()
